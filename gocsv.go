4ea8c60f244eababa15688e22774fe3ac71f2576
Ximarin <tobias.seidler@googlemail.com>
2013-02-26 20:10:00 +0100

    Added basic file reading

--- gocsv.go
+++ gocsv.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	ReadCSV("test", "x")
+	ReadCSV("test.csv", "x")
 }
 
-func ReadCSV(filename string, delimiter string) {
+func ReadCSV(filename string, delimiter byte) {
 	fmt.Println("Trying to use file " + filename + " as csv with " + delimiter + " as delimiter")
+
+	//try reading file, defer close
+	fileHandle, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("file not found")
+		os.Exit(99)
+	}
+	defer fileHandle.Close()
 
 }
